6c4273283b1b276e0294b37801c96a51aa401aed
Alex <86987255+AlexDeFoc@users.noreply.github.com>
2024-02-10 20:01:08 +0200

    updated new user script

--- New_User.py
+++ New_User.py
@@ -1,33 +1,21 @@
 import os
 import shutil
 
-def create_unique_user_folder_and_copy_files():
-    # Get the current directory
-    current_directory = os.getcwd()
-
-    # Generate a unique folder name by incrementing a counter
-    user_number = 1
-    while True:
-        folder_name = f"User_{user_number}"
-
-        # Check if the folder already exists
-        if not os.path.exists(os.path.join(current_directory, folder_name)):
-            # Create the new folder
-            os.makedirs(os.path.join(current_directory, folder_name))
-            break
-
-        # Increment the counter
-        user_number += 1
-
-    # Get the current user's home directory
-    current_user_home_directory = os.path.join(os.path.expanduser('~'), 'AppData', 'Roaming', 'Spotify')
+def new_save():
+    # Get script location
+    save_folder = os.path.join(os.getcwd(), 'User')
+    # Get file path from spotify folder
+    save_file_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Roaming', 'Spotify', 'prefs')
+    # Get source folder path
+    source_path = os.path.join(os.path.join(os.getcwd(), 'Source'))
+    # save_folder_source = os.path.join(save_folder, 'Source')
 
     # Copy the prefs file and source files
-    shutil.copy(os.path.join(current_user_home_directory, 'prefs'), os.path.join(current_directory, folder_name))
-    for file in os.listdir('Source'):
-        source_file_path = os.path.join('Source', file)
-        destination_file_path = os.path.join(os.path.join(current_directory, folder_name), file)
-        shutil.copy(source_file_path, destination_file_path)
-
-if __name__ == "__main__":
-    create_unique_user_folder_and_copy_files()
+    if not os.path.exists(os.path.join(save_folder)):
+        os.makedirs(save_folder)
+        print("starting copy")
+        shutil.copy(save_file_path, save_folder)
+        shutil.copy(os.path.join(source_path, 'Copy.py'), save_folder)
+        shutil.copy(os.path.join(source_path, 'Switch.bat'), save_folder)
+    
+new_save()
